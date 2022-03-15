1166fcb7a83283dd6d93daa82b1814aa6b92b306
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2022-03-15 20:40:48 +0100

    Parse csv file and populate db

--- hot_coffee_brewers/tables.py
+++ hot_coffee_brewers/tables.py
@@ -1,23 +1,28 @@
 def createCoffeeTable():
-  return "CREATE TABLE `coffeeShop` (" \
-                      " `id` VARCHAR(255) NOT NULL," \
-                      " `name` VARCHAR(255)," \
-                      " `country` VARCHAR(255)," \
-                      ") ENGINE=InnoDB" 
-                      
+    return "CREATE TABLE `coffeeShop` (" \
+        " `id` VARCHAR(255) NOT NULL," \
+        " `name` VARCHAR(255) NOT NULL," \
+        " `country` VARCHAR(255) NOT NULL," \
+        "  PRIMARY KEY (`id`) NOT NULL" \
+        ") ENGINE=InnoDB"
+
+
 def createCoffeeShopTable():
-  return "CREATE TABLE `review` (" \
-                      " `id` INT NOT NULL," \
-                      " `date` VARCHAR(255)," \
-                      " `rating` INT," \
-                      " `coffeeName` INT," \
-                      " `shopId` VARCHAR(255)," \
-                      ") ENGINE=InnoDB" 
-                      
+    return "CREATE TABLE `review` (" \
+        " `id` INT NOT NULL," \
+        " `date` VARCHAR(255) NOT NULL," \
+        " `rating` INT NOT NULL," \
+        " `coffeeName` INT NOT NULL," \
+        " `shopId` VARCHAR(255) NOT NULL," \
+        "  PRIMARY KEY (`id`) NOT NULL" \
+        ") ENGINE=InnoDB"
+
+
 def createReviewsTable():
-  return "CREATE TABLE `coffee` (" \
-                      " `id` INT NOT NULL," \
-                      " `name` VARCHAR(255)," \
-                      " `roast` VARCHAR(255)," \
-                      " `origin` VARCHART(255)," \
-                      ") ENGINE=InnoDB" 
+    return "CREATE TABLE `coffee` (" \
+        " `id` INT NOT NULL," \
+        " `name` VARCHAR(255) NOT NULL," \
+        " `roast` VARCHAR(255) NOT NULL," \
+        " `origin` VARCHART(255) NOT NULL," \
+        "  PRIMARY KEY (`id`) NOT NULL" \
+        ") ENGINE=InnoDB"
